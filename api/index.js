149f8f0509cbc4c729e6d9a450d8e7430b2e6962
knas1 <83686663+knas1@users.noreply.github.com>
2023-11-24 17:33:12 +0300

    Add files via upload

--- api/index.js
+++ api/index.js
@@ -1,20 +1,15 @@
-
 const express = require("express");
-
 const cors = require("cors");
-
 const mongoose = require("mongoose");
 const app = express();
 const bcrypt = require("bcryptjs");
-const User= require("./models/user")
+const User = require("./models/user");
 require("dotenv").config();
 const salt = bcrypt.genSaltSync(10);
 const CookieParser = require("cookie-parser");
 
 app.use(express.json());
 app.use(CookieParser());
-const cron = require("node-cron");
-app.listen(4000)
 
 app.use(
   cors({
@@ -23,32 +18,30 @@ app.use(
   })
 );
 
-mongoose.connect(process.env.MONGOOSE_URL)
+mongoose.connect(process.env.MONGOOSE_URL);
 
 mongoose.connect(process.env.MONGOOSE_URL).then(() => {
-  console.log('Successfully connected to MongoDB Atlas!');
+  console.log("Successfully connected to MongoDB Atlas!");
 }).catch(err => {
-  console.error('Error connecting to MongoDB Atlas:', err.message);
+  console.error("Error connecting to MongoDB Atlas:", err.message);
 });
+
 app.get("/test", (req, res) => {
   res.json("test ok");
 });
 
 app.post("/register", async (req, res) => {
-  mongoose.connect(process.env.MONGOOSE_URL)
-
-  const {
-    firstName,
-    lastName,
-    email,
-    phoneNumber,
-    gender,
-    birthdate,
-    password,
-    
-  } = req.body;
-
   try {
+    const {
+      firstName,
+      lastName,
+      email,
+      phoneNumber,
+      gender,
+      birthdate,
+      password,
+    } = req.body;
+
     const userInstance = await User.create({
       firstName,
       lastName,
@@ -57,16 +50,43 @@ app.post("/register", async (req, res) => {
       gender,
       birthdate,
       password: bcrypt.hashSync(password, salt),
-     
     });
+
     res.json(userInstance);
   } catch (e) {
     if (e.code === 11000) {
-        const field = Object.keys(e.keyPattern)[0];
-        res.status(422).json({ message: `The ${field} is already in use.` });
+      const field = Object.keys(e.keyPattern)[0];
+      res.status(422).json({ message: `The ${field} is already in use.` });
     } else {
-        res.status(422).json({ message: e.message });
+      res.status(422).json({ message: e.message });
     }
   }
 });
 
+app.post("/login", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      return res.status(401).json({ message: "Invalid credentials" });
+    }
+
+    const passwordMatch = bcrypt.compareSync(password, user.password);
+
+    if (!passwordMatch) {
+      return res.status(401).json({ message: "Invalid credentials" });
+    }
+
+
+    res.json({ success: true, message: "Login successful" });
+  } catch (e) {
+    console.error("Login Error:", e);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+});
+
+app.listen(4000, () => {
+  console.log("Server is running on port 4000");
+});
