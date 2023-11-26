ffcbaac8a84c54743ae35d01b2455b1329a53df8
Muayad <zrufay@gmail.com>
2023-11-26 18:31:17 +0300

    add quizes

---
+++ api/models/quizSchema.js
@@ -0,0 +1,56 @@
+const mongoose = require('mongoose');
+
+const questionSchema = new mongoose.Schema({
+  questionText: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  correctAnswer: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  incorrectAnswers: [{
+    type: String,
+    required: true,
+    trim: true
+  }]
+});
+
+const quizSchema = new mongoose.Schema({
+    title: {
+      type: String,
+      required: true,
+      trim: true
+    },
+    difficulty: {
+      type: String,
+      required: true,
+      trim: true
+    },
+    category: {
+      type: String,
+      required: true,
+      trim: true,
+      enum: [
+        'General Knowledge', 'Entertainment: Books', 'Entertainment: Film', 
+        'Entertainment: Music', 'Entertainment: Musicals & Theatres', 
+        'Entertainment: Television', 'Entertainment: Video Games', 
+        'Entertainment: Board Games', 'Science & Nature', 'Science: Computers', 'Information Technology',
+        'Science: Mathematics', 'Mythology', 'Sports', 'Geography', 
+        'History', 'Politics', 'Art', 'Celebrities', 'Animals'
+      ]
+    },
+    questions: [questionSchema],
+    timer: {
+      type: Number,
+      required: true
+    },
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
+        ref: 'User' 
+      }
+  });
+module.exports = mongoose.model('Quiz', quizSchema);
--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,9 +7,9 @@ import Layout from "./Layout";
 import axios from "axios";
 import Homepage from "./pages/Homepage";
 import { UserContextProvider } from "./UserContextProvider";
-import myquizzes from "./pages/quiz/myquizzes";
 import CommunityQuizzes from "./pages/quiz/communityquizzes";
 import ApiQuizzes from "./pages/quiz/apiquizzes";
+import MyQuizzes from "./pages/quiz/myquizzes";
 axios.defaults.baseURL="http://localhost:4000"
 axios.defaults.withCredentials=true
 function App() {
@@ -19,7 +19,7 @@ function App() {
     <Routes>
       <Route path="/" element={<Layout/>}>
         <Route path="/home" element={<Homepage />} />
-        <Route path="/myquizzes"element={<myquizzes />} /> 
+        <Route path="/myquizzes"element={<MyQuizzes/>} /> 
         <Route path="/communityquizzes" element={<CommunityQuizzes />} /> 
         <Route path="/apiquizzes" element={<ApiQuizzes />} /> 
 
