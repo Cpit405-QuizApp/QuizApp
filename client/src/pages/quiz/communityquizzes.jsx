22676d65443a2f5c85aa505254aa2e8d1c32edad
knas1 <83686663+knas1@users.noreply.github.com>
2023-11-29 04:51:43 +0300

    categorize by difficulty and category

--- client/src/pages/quiz/communityquizzes.jsx
+++ client/src/pages/quiz/communityquizzes.jsx
@@ -1,26 +1,86 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import { Link } from "react-router-dom"; // Import Link from react-router-dom
+import { Link } from "react-router-dom";
 
 const CommunityQuizzes = () => {
   const [quizzes, setQuizzes] = useState([]);
+  const [filter, setFilter] = useState({ difficulty: "", category: "" });
+
+  const fetchQuizzes = async () => {
+    try {
+      const response = await axios.get("/quizzes/community", {
+        params: filter, // Pass filter parameters to the server
+      });
+      setQuizzes(response.data);
+    } catch (error) {
+      console.error("Error fetching community quizzes:", error);
+    }
+  };
 
   useEffect(() => {
-    const fetchQuizzes = async () => {
-      try {
-        const response = await axios.get("/quizzes/community");
-        setQuizzes(response.data);
-      } catch (error) {
-        console.error("Error fetching community quizzes:", error);
-      }
-    };
-
     fetchQuizzes();
-  }, []);
+  }, [filter]); // Refetch quizzes when the filter changes
+
+  const handleFilterChange = (event) => {
+    const { name, value } = event.target;
+    setFilter((prevFilter) => ({ ...prevFilter, [name]: value }));
+  };
 
   return (
     <div className="max-w-md mx-auto p-6 bg-gray-100 rounded-md shadow-md Baskervville min-h-screen flex flex-col justify-start items-center py-8">
       <h2 className="text-3xl font-bold mb-8 text-center">Community Quizzes</h2>
+      <div className="mb-4">
+        <label className="font-bold mr-2">Difficulty:</label>
+        <select
+          name="difficulty"
+          value={filter.difficulty}
+          onChange={handleFilterChange}
+        >
+          <option value="">All</option>
+          <option value="easy">Easy</option>
+          <option value="medium">Medium</option>
+          <option value="hard">Hard</option>
+        </select>
+      </div>
+      <div className="mb-4">
+      <label className="font-bold mr-2">Category:</label>
+      <select
+        name="category"
+        value={filter.category}
+        onChange={handleFilterChange}
+      >
+        <option value="">Select Category</option>
+        <option value="General Knowledge">General Knowledge</option>
+        <option value="Entertainment: Books">Entertainment: Books</option>
+        <option value="Entertainment: Film">Entertainment: Film</option>
+        <option value="Entertainment: Music">Entertainment: Music</option>
+        <option value="Entertainment: Musicals & Theatres">
+          Entertainment: Musicals & Theatres
+        </option>
+        <option value="Entertainment: Television">
+          Entertainment: Television
+        </option>
+        <option value="Entertainment: Video Games">
+          Entertainment: Video Games
+        </option>
+        <option value="Entertainment: Board Games">
+          Entertainment: Board Games
+        </option>
+        <option value="Science & Nature">Science & Nature</option>
+        <option value="Information Technology">Information Technology</option>
+        <option value="Science: Computers">Science: Computers</option>
+        <option value="Science: Mathematics">Science: Mathematics</option>
+        <option value="Mythology">Mythology</option>
+        <option value="Sports">Sports</option>
+        <option value="Geography">Geography</option>
+        <option value="History">History</option>
+        <option value="Politics">Politics</option>
+        <option value="Art">Art</option>
+        <option value="Celebrities">Celebrities</option>
+        <option value="Animals">Animals</option>
+      </select>
+    </div>
+
       <ul className="w-full max-w-md">
         {quizzes.map((quiz) => (
           <li
@@ -37,7 +97,6 @@ const CommunityQuizzes = () => {
             <div>
               <span className="font-bold">Time:</span> {quiz.timer} seconds
             </div>
-            {/* Link to the quiz taker page with the quiz ID */}
             <Link to={`/quiztaker/${quiz._id}`}>
               <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-4">
                 Take Quiz
