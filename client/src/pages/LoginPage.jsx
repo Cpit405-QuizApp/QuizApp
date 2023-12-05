594245aed1fc5cc4072ed0aacb7705fea09c5816
Muayad <zrufay@gmail.com>
2023-12-05 20:14:57 +0300

    some front edits

--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -2,6 +2,7 @@ import React, { useState,useContext } from "react";
 import { Link, useNavigate} from "react-router-dom";
 import axios from "axios";
 import { UserContext } from "../UserContextProvider";
+import styles from './IndexPage.module.css'; // Make sure the path is correct
 
 export default function LoginPage() {
   const [email, setEmail] = useState("");
@@ -24,19 +25,13 @@ export default function LoginPage() {
   }
 
   return (
-    <div>
-      <form
-        onSubmit={login}
-        className="w-full max-w-md bg-white p-8 rounded-lg bg-opacity-50 shadow-md"
-      >
-        <h1 className="text-4xl font-bold mb-8 text-center">Login</h1>
+    <div className="flex justify-center items-center min-h-screen bg-gray-100">
+      <form onSubmit={login} className="w-full max-w-md bg-white p-8 rounded-lg shadow-md border border-black">
+        <h1 className="text-4xl font-bold mb-8 text-center text-black">Login</h1>
 
         {/* Email */}
         <div className="mb-4">
-          <label
-            htmlFor="email"
-            className="block text-sm font-medium text-gray-700"
-          >
+          <label htmlFor="email" className="block text-sm font-medium text-black">
             Email
           </label>
           <input
@@ -44,7 +39,7 @@ export default function LoginPage() {
             type="email"
             required
             placeholder="example@gmail.com"
-            className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500"
+            className="mt-1 block w-full px-3 py-2 border border-black rounded-md shadow-sm focus:ring-black focus:border-black"
             value={email}
             onChange={(e) => setEmail(e.target.value)}
           />
@@ -52,32 +47,31 @@ export default function LoginPage() {
 
         {/* Password */}
         <div className="mb-6">
-          <label
-            htmlFor="password"
-            className="block text-sm font-medium text-gray-700"
-          >
+          <label htmlFor="password" className="block text-sm font-medium text-black">
             Password
           </label>
           <input
             id="password"
             type="password"
+            placeholder="*************"
+
             required
-            className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500"
+            className="mt-1 block w-full px-3 py-2 border border-black rounded-md shadow-sm focus:ring-black focus:border-black"
             value={password}
             onChange={(e) => setPassword(e.target.value)}
           />
         </div>
 
-        <button
-          type="submit"
-          className="w-full bg-blue-500 text-white py-2 px-4 rounded-md hover:bg-blue-600"
-        >
+        {/* Login Button */}
+        <button type="submit" className={`w-full py-2 px-4 rounded-md ${styles.loginButton}`}>
           Login
         </button>
+
+        {/* Sign Up Link */}
         <div className="flex justify-center mt-2">
-          <span className="primary">
+          <span className="text-black">
             Don't have an account?{" "}
-            <Link to="/register" className="underline">
+            <Link to="/register" className="underline text-black hover:text-opacity-70">
               Sign up
             </Link>
           </span>
--- client/src/pages/indexpage.jsx
+++ client/src/pages/indexpage.jsx
@@ -1,29 +1,26 @@
+import React from 'react';
 import { Link } from "react-router-dom";
+import logo from "../assets/img/logo.png";
+import styles from './IndexPage.module.css'; // Update the path to where your CSS module is located
 
-export default function IndexPage(){
+export default function IndexPage() {
+  return (
+    <div className="min-h-screen flex flex-col items-center justify-center bg-gray-100 py-8">
+      {/* Logo */}
+      <img src={logo} alt="Quiz App Logo" className="mb-10" style={{ maxWidth: '300px' }} />
 
+      {/* Welcome Message */}
+      <h1 className="text-3xl font-bold mb-6 text-center" style={{ color: '#2C2A2D' }}>Welcome to our Quiz app</h1>
 
-return(
-  <div className="min-h-screen flex flex-col justify-start items-center py-8 ">
-  <h1 className="text-3xl font-bold mb-4 text-center">Welcome to our Quiz app</h1>
-  <div className="mb-2">
-    <Link to ={'/login'} className="border border-purple-600
-       text-purple-600 bg-white px-6 py-2 rounded-md 
-       font-semibold shadow-md transition duration-300 hover:bg-purple-600
-        hover:text-white hover:shadow-lg"
-    >
-      Login
-    </Link>
-    <Link to ={'/register'} className="border border-purple-600
-       text-purple-600 bg-white px-6 py-2 rounded-md 
-       font-semibold shadow-md transition duration-300 hover:bg-purple-600
-        hover:text-white hover:shadow-lg"
-    >
-      Sign up
-    </Link>
-  </div>
-</div>
-
-)
-
+      {/* Navigation Buttons */}
+      <div className="flex flex-row justify-center items-center gap-4">
+        <Link to={'/login'} className={`px-6 py-2 rounded-md font-semibold shadow-md transition duration-300 ${styles.loginButton}`}>
+          Login
+        </Link>
+        <Link to={'/register'} className={`px-6 py-2 rounded-md font-semibold shadow-md transition duration-300 ${styles.signupButton}`}>
+          Sign up
+        </Link>
+      </div>
+    </div>
+  );
 }
